0fe2614978e625ab6df7f69ca13ceec2f8ea8eae
sshakti93 <siddhishakti31@gmail.com>
2024-06-06 16:58:26 +0530

    feat: adds other pages to be displayed

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,10 @@ import "./page.css"
 import { Header } from "./Components/Header/Header";
 import { Hero } from "./Components/Hero/Hero";
 import { Footer } from "./Components/Footer/Footer";
+import Circlearchi from "./Components/CircleArchiTexture/Circlearchi";
+import Search from "./Components/Searchpage/Search";
+import LoadingCollectibles from "./Components/LoadingCollectibles/LoadingCollectibles";
+import Viewauctions from "./Components/Viewacutions/Viewauctions";
 
 
 export default function Home() {
@@ -14,7 +18,12 @@ export default function Home() {
 return(
   <>
   <Header/>
-  <Hero/>
+  <Viewauctions/>
+  {/* <Search/> */}
+  {/* <LoadingCollectibles/> */}
+  {/* <Hero/> */}
+  {/* <Circlearchi/> */}
+
   <Footer/>
   </>
 )
