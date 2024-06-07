6bc597c51ae9737ce85de6dce669a153398100b7
sshakti93 <siddhishakti31@gmail.com>
2024-06-07 12:25:04 +0530

    fixe: adds fixes to the styling and formatting of the artifact page

--- app/Components/CircleArchiTexture/Circlearchi.tsx
+++ app/Components/CircleArchiTexture/Circlearchi.tsx
@@ -74,42 +74,141 @@ export default function Circlearchi() {
 
             <div className='right-prod-details'>
                 
-                
-                <div className='bids'>
-                    <div className='current-bid'>
-                        <p>Current bid</p>
+                <div className='right-top'>
+                    <div className='bids'>
+                        <div className='current-bid'>
+                            <p>Current bid</p>
+                        </div>
+                        <div className='bid-ending'>
+                            <p>Bid ending in</p>
+                        </div>
                     </div>
-                    <div className='bid-ending'>
-                        <p>Bid ending in</p>
+
+                    <div className='bid-nft-time'>
+                        <div className='bid-nft'>
+                            <p>8.38 ETH</p>
+                        </div>
+                        <div className='bid-time'>
+                            <p>8</p>
+                            <p>15</p>
+                            <p>29</p>
+                        </div>
+                    </div>
+
+                    <div className='bid-value-time'>
+                        <div className='bid-value'>
+                            <p>=$25,775</p>
+                        </div>
+                        <div className='bid-time-words'>
+                            <p>Hours</p>
+                            <p>Mins</p>
+                            <p>Secs</p>
+                        </div>
+
+                        
+                    </div>
+
+                    <div className='place-bid'>
+                        <button>Place your bid now</button>
                     </div>
                 </div>
-
-                <div className='bid-nft-time'>
-                    <div className='bid-nft'>
-                        <p>8.38 ETH</p>
-                    </div>
-                    <div className='bid-time'>
-                        <p>8</p>
-                        <p>15</p>
-                        <p>29</p>
-                    </div>
-                </div>
-
-                <div className='bid-value-time'>
-                    <div className='bid-value'>
-                        <p>=$25,775</p>
-                    </div>
-                    <div className='bid-time-words'>
-                        <p>Hours</p>
-                        <p>Mins</p>
-                        <p>Secs</p>
-                    </div>
 
                     
-                </div>
+                <div className='right-bottom'>
+                    <div className='first-bidder'>
+                        <div className='left-placedby'>
+                            <div className='placedby-img'>
+                                <img src='/images/placedby_user1.svg'/>
 
-                    
-                
+                            </div>
+                            <div className='placedby-details'>
+                                <div className='placeby-sno'>
+                                    <p>Bid placed by #114BV32</p>
+                                </div>
+                                <div className='placedby-date'>
+                                    <p>Mar 26 2021 at 4.59 PM</p>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div className='right-placedby'>
+                            <div className='placed-nft-cost'>
+                                <p>5.69 ETH</p>
+
+                            </div>
+                            <div className='placed-nft-curr'>
+                                <p>= $10,889</p>
+
+                            </div>
+                        </div>
+                        
+
+                    </div>
+                    <div className='second-bidder'>
+
+                    <div className='left-placedby'>
+                            <div className='placedby-img'>
+                                <img src='/images/placedby_user2.svg'/>
+
+                            </div>
+                            <div className='placedby-details'>
+                                <div className='placeby-sno'>
+                                    <p>Bid placed by #23AS88</p>
+                                </div>
+                                <div className='placedby-date'>
+                                    <p>Mar 26 2021 at 3.21 PM</p>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div className='right-placedby'>
+                            <div className='placed-nft-cost'>
+                                <p>6.99 ETH</p>
+
+                            </div>
+                            <div className='placed-nft-curr'>
+                                <p>= $12,289</p>
+
+                            </div>
+                        </div>
+                        
+
+
+                    </div>
+                    <div className='third-bidder'>
+
+                    <div className='left-placedby'>
+                            <div className='placedby-img'>
+                                <img src='/images/placedby_user3.svg'/>
+
+                            </div>
+                            <div className='placedby-details'>
+                                <div className='placeby-sno'>
+                                    <p>Bid placed by #57PS22</p>
+                                </div>
+                                <div className='placedby-date'>
+                                    <p>Mar 26 2021 at 3.18 PM</p>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div className='right-placedby'>
+                            <div className='placed-nft-cost'>
+                                <p>3.99 ETH</p>
+
+                            </div>
+                            <div className='placed-nft-curr'>
+                                <p>= $8,339</p>
+
+                            </div>
+                        </div>
+                        
+
+                    </div>
+                </div>
             </div>
 
         </div>
