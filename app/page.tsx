91bce14014456c3c0450ca44abce02b5cbbf198c
sshakti93 <siddhishakti31@gmail.com>
2024-06-07 12:26:07 +0530

    fix: prevents unwanted AuctionList page from being rendered on the web page

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,30 @@ import "./page.css"
 
 
 import { Header } from "./Components/Header/Header";
-import { Hero } from "./Components/Hero/Hero";
 import { Footer } from "./Components/Footer/Footer";
 import Circlearchi from "./Components/CircleArchiTexture/Circlearchi";
 import Search from "./Components/Searchpage/Search";
 import LoadingCollectibles from "./Components/LoadingCollectibles/LoadingCollectibles";
 import Viewauctions from "./Components/Viewacutions/Viewauctions";
+import Navbar from "./Components/Navbar/Navbar";
+import FullAuctionList from "./Components/FullAuctionList/FullAuctionList";
+import SellOnAuction from "./Components/SellOnAuction/SellOnAuction";
 
 
 export default function Home() {
   
 return(
   <>
+  
   <Header/>
+
   {/* <Viewauctions/> */}
-  {/* <Search/> */}
   {/* <LoadingCollectibles/> */}
-  <Hero/>
+  {/* <FullAuctionList/> */}
+  
+  {/* <SellOnAuction/> */}
   {/* <Circlearchi/> */}
+  {/* <Search/> */}
 
   <Footer/>
   </>
