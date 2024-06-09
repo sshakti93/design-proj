5559fc39ff87415d8c3f5df6d842cb523466b838
sshakti93 <siddhishakti31@gmail.com>
2024-06-09 23:00:48 +0530

    feat: adds references to the pages

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,15 @@ import Viewauctions from "./Components/Viewacutions/Viewauctions";
 import Navbar from "./Components/Navbar/Navbar";
 import FullAuctionList from "./Components/FullAuctionList/FullAuctionList";
 import SellOnAuction from "./Components/SellOnAuction/SellOnAuction";
-
+import Create from "./Components/Create/Create";
+import CreatePreview from "./Components/CreatePreview/CreatePreview";
+import ConnectWallet from "./Components/SmallPages/ConnectWallet/ConnectWallet";
+import Account from "./Components/SmallPages/Account/Account";
+import Tos from "./Components/SmallPages/TOS/Tos";
+import BidForm from "./Components/SmallPages/BidForm/BidForm";
+import SellCard from "./Components/SmallPages/SellCard/SellCard";
+import Success from "./Components/SmallPages/Success/Success";
+import Uploading from "./Components/SmallPages/Uploading/Uploading";
 
 export default function Home() {
   
@@ -27,10 +35,34 @@ return(
   {/* <FullAuctionList/> */}
   
   {/* <SellOnAuction/> */}
-  {/* <Circlearchi/> */}
-  {/* <Search/> */}
 
+
+  {/* work on... */}
+  {/* <Circlearchi/>*/}
+  {/* <Create/> */}
+  {/* <CreatePreview/> */}
+  
+  {/* <Search/> */}
+  
+  
+
+   {/* Smaller pop-ups */}
+
+   {/* work... */}
+   {/* <BidForm/> */}
+  {/* <SellCard/> */}
+
+
+  
+  
+  {/* <ConnectWallet/> */}
+  {/* <Account/> */}
+  {/* <Tos/> */}
+  {/* <Success/> */}
+  {/* <Uploading/> */}
   <Footer/>
+
+
   </>
 )
 
